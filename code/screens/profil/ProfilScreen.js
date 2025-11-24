cfbd0748d1ee16c297ad49fa5765e1304b07d184
MonsieurClavett <alexmarcouiller9@gmail.com>
2025-11-24 18:01:04 -0500

    On peut naviguer sur le profil d'un user en cliquant sur son avis

--- code/screens/profil/ProfilScreen.js
+++ code/screens/profil/ProfilScreen.js
@@ -13,7 +13,6 @@ import authService from "../../services/Auth";
 import { useTheme } from "/home/etd/Projet-Application-Mobile/code/context/ThemeContext.js";
 import MarthaService from "../../services/MarthaService.js";
 
-// --- Tes annonces mock ---
 const ANNONCES = [];
 
 export default function ProfilScreen({ navigation, route }) {
